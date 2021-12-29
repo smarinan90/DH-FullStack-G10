6e5104aef32e7851243d36f4bbb724b7515012f0
smarinan90 <smarinan90@gmail.com>
2021-12-28 20:53:08 -0500

    [ADD] General structure

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const publicPath = path.resolve(__dirname, "./public");
+
+app.use(express.static(publicPath));
+
+app.listen(3000, () => console.log("server running properly"));
+app.get("/", (req, res) => res.sendFile(path.resolve(__dirname, "./views/home.html")));
+
