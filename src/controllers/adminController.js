a4375bd52881b4336a6ea353ac96d16d77a383ea
smarinan90 <smarinan90@gmail.com>
2022-01-22 16:19:00 -0500

    [ADD] img folders + strutures

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,8 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsPath = path.join(__dirname, '../database/products.json');
+const productsList = JSON.parse(fs.readFileSync(productsPath, 'utf-8'));
+
 module.exports = {
     products_list: (req, res) => {
-        res.render('admin/products_list')
+        res.render('admin/products_list', {
+            productsList
+        })
     },
     product_edit: (req, res) => {
         res.render('admin/product_edit')
+    },
+    store: (req, res) => {
+        let newProduct = req.body;
+
+
+    },
+    edit: (req, res) => {
+        let id = req.param.id;
     }
 }
--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,8 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsPath = path.join(__dirname, '../database/products.json');
+const productsList = JSON.parse(fs.readFileSync(productsPath, 'utf-8'));
+
+
 module.exports = {
     products: (req, res) => {
-        res.render('client/products')
+
+        res.render('client/products', {
+            productsList
+        })
     },
     single_product: (req, res) => {
-        res.render('client/single_product')
+
+        let id = req.params.id;
+        let product = productsList.find(p => p.id == id)
+
+        res.render('client/single_product', {
+            product
+        })
     }
-};
+}
