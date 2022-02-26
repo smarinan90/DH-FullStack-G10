c008141546ec70db8480a1912364302259744392
Daniel Amaya B <daniel.amaya.buitrago@outlook.com>
2022-02-26 16:21:24 -0500

    [ADD] se agregó la validación del correo de register. Hay que validar rutas de user.js

--- src/routers/users.js
+++ src/routers/users.js
@@ -5,8 +5,38 @@ const { body } = require('express-validator');
 const userController = require('../controllers/userController');
 
 const validateCreateForm = [
-    body('first_name').notEmpty().withMessage('Complete el campo de nombre'),
-    body('last_name').notEmpty().withMessage('Complete el campo de apellido'),
-    body('email').isEmail().withMessage('Complete el campo con un correo valido'),
-
+    body('first_name')
+    .notEmpty()
+    .withMessage('Complete el campo de nombre')
+    .bail()
+    .isLength({ min: 1 })
+    .withMessage('Nombre muy corto'),
+    body('last_name')
+    .notEmpty()
+    .withMessage('Complete el campo de apellido')
+    .bail()
+    .isLength({ min: 1 })
+    .withMessage('Apellido muy corto'),
+    body('email')
+    .notEmpty()
+    .withMessage('Complete el correo')
+    .bail()
+    .isEmail()
+    .withMessage('Complete el campo con un correo valido'),
+    body('fechaNacimiento')
+    .notEmpty()
+    .withMessage('Complete la fecha de nacimiento')
+    .bail()
+    .isDate(),
+    body('contrasena_1')
+    .notEmpty()
+    .withMessage('Complete contraseña')
+    .bail()
+    .isLength({ min: 8, max: 12})
+    .withMessage('La contraseña debe tener entre 8 y 12 caracteres')
 ];
+
+
+/* router.get('/', userController.home);
+router.get('/login', userController.login);
+router.get('/register', userController.register); */
