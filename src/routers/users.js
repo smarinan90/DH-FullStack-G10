f00194eb4ba020e5217b859e2ec18ded4d731027
Daniel Amaya B <daniel.amaya.buitrago@outlook.com>
2022-02-26 15:53:58 -0500

    [ADD UPDATE] se agregó en users.js la validación del formulario para first_name, last_name y email

--- src/routers/users.js
+++ src/routers/users.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { body } = require('express-validator');
+
 const userController = require('../controllers/userController');
 
 const validateCreateForm = [
-    body('first_name').notEmpty().withMessage('Complete el campo de nombre');
-    body('last_name').notEmpty().withMessage('Complete el campo de apellido');
-    body('email').isEmail().withMessage('Complete el campo con un correo valido');
+    body('first_name').notEmpty().withMessage('Complete el campo de nombre'),
+    body('last_name').notEmpty().withMessage('Complete el campo de apellido'),
+    body('email').isEmail().withMessage('Complete el campo con un correo valido'),
 
 ];
