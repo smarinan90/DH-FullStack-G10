26e36f217ad27c968bf6b3e1a96c54555c92dade
juliant908 <juliant908@gmail.com>
2022-02-28 19:00:18 -0500

    Redirect funcionando

--- src/routers/mainRouter.js
+++ src/routers/mainRouter.js
@@ -1,51 +1,50 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { body } = require('express-validator');
-const path = require('path');
+const { body } = require("express-validator");
+const path = require("path");
 
-const mainController = require('../controllers/mainController');
-
+const mainController = require("../controllers/mainController");
 
 // Express-validator
 
 const validator = [
-    body('first_name')
-        .notEmpty()
-        .withMessage('Complete el campo de nombre')
-        .bail()
-        .isLength({ min: 1 })
-        .withMessage('Nombre muy corto'),
-    body('last_name')
-        .notEmpty()
-        .withMessage('Complete el campo de apellido')
-        .bail()
-        .isLength({ min: 1 })
-        .withMessage('Apellido muy corto'),
-    body('email')
-        .notEmpty()
-        .withMessage('Complete el correo')
-        .bail()
-        .isEmail()
-        .withMessage('Complete el campo con un correo valido'),
-    body('birth_date')
-        .notEmpty()
-        .withMessage('Complete la fecha de nacimiento')
-        .bail()
-        .isDate(),
-    body('password')
-        .notEmpty()
-        .withMessage('Complete contraseña')
-        .bail()
-        .isLength({ min: 8, max: 12 })
-        .withMessage('La contraseña debe tener entre 8 y 12 caracteres')
+  body("first_name")
+    .notEmpty()
+    .withMessage("Complete el campo de nombre")
+    .bail()
+    .isLength({ min: 1 })
+    .withMessage("Nombre muy corto"),
+  body("last_name")
+    .notEmpty()
+    .withMessage("Complete el campo de apellido")
+    .bail()
+    .isLength({ min: 1 })
+    .withMessage("Apellido muy corto"),
+  body("email")
+    .notEmpty()
+    .withMessage("Complete el correo")
+    .bail()
+    .isEmail()
+    .withMessage("Complete el campo con un correo valido"),
+  body("birth_date")
+    .notEmpty()
+    .withMessage("Complete la fecha de nacimiento")
+    .bail()
+    .isDate(),
+  body("password")
+    .notEmpty()
+    .withMessage("Complete contraseña")
+    .bail()
+    .isLength({ min: 8, max: 12 })
+    .withMessage("La contraseña debe tener entre 8 y 12 caracteres"),
 ];
 
 // Main Routes
 
-router.get('/', mainController.home);
-router.get('/register', mainController.register);
-router.post('/register', mainController.createUser);
-router.get('/login', mainController.login);
+router.get("/", mainController.home);
+router.get("/register", mainController.register);
+router.post("/register", validator, mainController.createUser);
+router.get("/login", mainController.login);
 // router.post('/artist', mainController.)
 
 // Export
