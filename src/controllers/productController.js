3f9d5f8ad4249e504c5c08f4786658c1745c92f5
juliant908 <juliant908@gmail.com>
2022-03-07 22:30:09 -0500

    [UPDATE] enrutado de producto, correcciones en las impresiones de EJS, correcciones en el display de los géneros, correciones en el enrutado de los partials y CSS

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,33 +1,32 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const productsPath = path.join(__dirname, '../database/products.json');
-const productsList = JSON.parse(fs.readFileSync(productsPath, 'utf-8'));
-
+const productsPath = path.join(__dirname, "../database/products.json");
+const productsList = JSON.parse(fs.readFileSync(productsPath, "utf-8"));
 
 module.exports = {
-    products: (req, res) => {
+  products: (req, res) => {
+    let genres = [];
+    productsList.forEach((el) => {
+      genres.includes(el.genre) ? genres : genres.push(el.genre);
+    });
 
-        let genres = [];
-        productsList.forEach(el => {
-            genres.includes(el.genre) ? genres : genres.push(el.genre); 
-        });
+    let genreFilter = req.query.genre;
+    let listOfProducts = req.query.genre
+      ? productsList.filter((p) => p.genre == genreFilter)
+      : productsList;
 
-        let genreFilter = req.query.genre;
-        let listOfProducts = req.query.genre ? productsList.filter(p => p.genre == genreFilter) : productsList;
+    res.render("client/products", {
+      listOfProducts,
+      genres,
+    });
+  },
+  single_product: (req, res) => {
+    let id = req.params.id;
+    let product = productsList.find((p) => p.id == id);
 
-        res.render('client/products', {
-            listOfProducts,
-            genres
-        })
-    },
-    single_product: (req, res) => {
-
-        let id = req.params.id;
-        let product = productsList.find(p => p.id == id)
-
-        res.render('client/single_product', {
-            product
-        })
-    }
-}
+    res.render("client/single_product", {
+      product,
+    });
+  },
+};
