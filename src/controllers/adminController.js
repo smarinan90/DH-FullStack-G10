91438fb540d23f81b1a9046d79609397e6a9c5bc
smarinan90 <smarinan90@gmail.com>
2022-03-29 10:55:05 -0500

    [ADD] CRUD try caatch

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,4 +1,3 @@
-const req = require("express/lib/request");
 const db = require("../../database/models");
 
 module.exports = {
@@ -11,99 +10,125 @@ module.exports = {
         });
     },
 
+    // Artist CRUD
+
     artist_creation_page: (req, res) => {
         res.render('admin/artist_create');
     },
 
     create_artist: async (req, res) => {
-        const valResult = validationResult(req);
-        if (!valResult.errors) {
-            return res.render("admin/artist_create", {
-                errors: valResult.mapped(),
-                oldData: req.body,
-            });
-        } else {
+        try {
+            const valResult = validationResult(req);
+            if (!valResult.errors) {
+                return res.render("admin/artist_create", {
+                    errors: valResult.mapped(),
+                    oldData: req.body,
+                });
+            } else {
 
-            const { name, banner, artist_picture, description } = req.body;
+                const { name, banner, artist_picture, description } = req.body;
 
-            let nameVerification = await db.Users.findOne({
-                where: { name: name }
-            });
+                let nameVerification = await db.Users.findOne({
+                    where: { name: name }
+                });
 
-            nameVerification ? res.render("admin/artist_create", {
-                errors: {
-                    email: {
-                        msg: 'El artista ya existe'
-                    }
-                },
-                oldData: req.body,
-            }) : null;
+                nameVerification ? res.render("admin/artist_create", {
+                    errors: {
+                        email: {
+                            msg: 'El artista ya existe'
+                        }
+                    },
+                    oldData: req.body,
+                }) : null;
 
-            const newArtist = {
-                name,
-                banner,
-                artist_picture,
-                description
+                const newArtist = {
+                    name,
+                    banner,
+                    artist_picture,
+                    description
+                }
+
+                await db.Artists.create(newArtist);
             }
 
-            await db.Artists.create(newArtist);
-        }
+            res.redirect("/admin/products_list");
 
-        res.redirect("/admin/products_list");
+        } catch (error) {
+            res.send(error)
+        }
     },
 
     artist_edit_page: async (req, res) => {
-        const id = req.params.id;
-        const artistInfo = await db.Artists.findByPk(id);
 
-        res.render('admin/artist_create', {
-            oldData: artistInfo
-        });
+        try {
+            const id = req.params.id;
+            const artistInfo = await db.Artists.findByPk(id);
+
+            res.render('admin/artist_create', {
+                oldData: artistInfo
+            });
+
+        } catch (error) {
+            res.send(error)
+        }
     },
 
     update_artist: async (req, res) => {
-        const valResult = validationResult(req);
-        if (!valResult.errors) {
-            return res.render("admin/artist_create", {
-                errors: valResult.mapped(),
-                oldData: req.body,
-            });
-        } else {
 
-            const { name, banner, artist_picture, description } = req.body;
+        try {
+            const valResult = validationResult(req);
+            if (!valResult.errors) {
+                return res.render("admin/artist_create", {
+                    errors: valResult.mapped(),
+                    oldData: req.body,
+                });
+            } else {
 
-            let nameVerification = await db.Users.findOne({
-                where: { name: name }
-            });
+                const { name, banner, artist_picture, description } = req.body;
 
-            nameVerification ? res.render("admin/artist_create", {
-                errors: {
-                    email: {
-                        msg: 'El artista ya existe'
-                    }
-                },
-                oldData: req.body,
-            }) : null;
+                let nameVerification = await db.Users.findOne({
+                    where: { name: name }
+                });
 
-            const newArtist = {
-                name,
-                banner,
-                artist_picture,
-                description
+                nameVerification ? res.render("admin/artist_create", {
+                    errors: {
+                        email: {
+                            msg: 'El artista ya existe'
+                        }
+                    },
+                    oldData: req.body,
+                }) : null;
+
+                const newArtist = {
+                    name,
+                    banner,
+                    artist_picture,
+                    description
+                }
+
+                await db.Artists.update(newArtist, { where: { id: req.params.id } });
             }
 
-            await db.Artists.update(newArtist, { where: { id: req.params.id } });
-        }
+            res.redirect("/admin/products_list");
 
-        res.redirect("/admin/products_list");
+        } catch (error) {
+            res.send(error)
+        }
     },
 
     delete_artist: async (req, res) => {
 
-        db.Artists.destroy({ where: { id: req.param.id } });
+        try {
+            db.Artists.destroy({ where: { id: req.param.id } });
 
-        res.redirect("/admin/products_list");
+            res.redirect("/admin/products_list");
+
+        } catch (error) {
+            res.send(error)
+        }
     },
+
+    // Album CRUD
 
 
 }
