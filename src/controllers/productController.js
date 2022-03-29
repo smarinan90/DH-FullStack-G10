c6a95465fd91802da475f22e26fb00ea5f54cd52
smarinan90 <smarinan90@gmail.com>
2022-03-28 22:27:14 -0500

    [FIX]  various fixes

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -3,7 +3,8 @@ const db = require("../../database/models");
 module.exports = {
     products: async (req, res) => {
         const artists = await db.Artists.findAll();
-        res.render("client/products", { artists })
+        const genres = await db.Genres.findAll();
+        res.render("client/products", { artists, genres })
     },
 
     single_product: async (req, res) => {
