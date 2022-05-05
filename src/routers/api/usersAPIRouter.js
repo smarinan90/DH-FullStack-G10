d16edc27efd3f9a726ebe22abbdb388d7d457804
smarinan90 <smarinan90@gmail.com>
2022-05-04 21:57:00 -0500

    [ADD] users API

--- src/routers/api/usersAPIRouter.js
+++ src/routers/api/usersAPIRouter.js
@@ -2,12 +2,8 @@ const express = require("express");
 const router = express.Router();
 const usersAPIController = require("../../controllers/api/usersAPIController");
 
-//Rutas
-//Listado de todos los generos
+
 router.get("/", usersAPIController.list);
-//Detalle del genero
-router.get("/:id", usersAPIController.detail);
-//Películas por genero
-router.get("/:id/movies", usersAPIController.genreMovies);
+
 
 module.exports = router;
